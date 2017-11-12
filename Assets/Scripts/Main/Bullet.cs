4ef791f1ad4979f8e6930b42dc2b2b86f69cd05a
Sest <sest.niconico@gmail.com>
2017-11-13 00:10:47 +0900

    Add BulletLender

---
+++ Assets/Scripts/Main/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoulCaptcha {
+    public class Bullet : MonoBehaviour {
+        public int power;
+
+        [HideInInspector]
+        public GameObject prefab;
+    }
+}
---
+++ Assets/Scripts/Main/BulletLender.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UniRx;
+
+namespace SoulCaptcha {
+    public class BulletLender : SingletonMonoBehaviour<BulletLender> {
+        public int bulletPreloadCount = 30;
+        public List<GameObject> bullets;
+
+        Dictionary<GameObject, BulletPool> pools = new Dictionary<GameObject, BulletPool>();
+
+        // Use this for initialization
+        protected override void Init () {
+            CreatePools(bullets, bulletPreloadCount);
+        }
+
+        void CreatePools(List<GameObject> prefabs, int preloadCount) {
+            foreach (GameObject prefab in prefabs) {
+                BulletPool pool = new BulletPool(prefab);
+                pool.PreloadAsync(preloadCount, 10).Subscribe();
+                pools.Add(prefab, pool);
+            }
+        }
+
+        public GameObject Rent(GameObject prefab) {
+            Bullet bo = pools[prefab].Rent();
+            bo.prefab = prefab;
+            return bo.gameObject;
+        }
+
+        public void Return(GameObject go) {
+            Bullet instance = go.GetComponent<Bullet>();
+            pools[instance.prefab].Return(instance);
+        }
+
+        public void Return(Bullet instance) {
+            pools[instance.prefab].Return(instance);
+        }
+    }
+}
